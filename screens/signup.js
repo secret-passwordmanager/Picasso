ffbdab69c88bae618ad9ff8c40b99b68f612f171
Roy Dey <roydey@ucdavis.edu>
2020-08-26 16:46:05 -0700

    create POST request to register user

--- screens/signup.js
+++ screens/signup.js
@@ -8,7 +8,7 @@ const styles = global_styles.css_styles;
 export default function signup_screen({navigation}){
     const [first_name, set_first_name] = useState('');
     const [last_name, set_last_name] = useState('');
-    const [email, set_email] = useState('');
+    const [master_cred, set_master_cred] = useState('');
     const [username, set_username] = useState('');
     const [password, set_password] = useState('');
 
@@ -33,14 +33,6 @@ export default function signup_screen({navigation}){
 
             <View style={styles.login_field}>
                 <TextInput 
-                style={styles.input_text}
-                placeholder='Email' 
-                placeholderTextColor={global_styles.background_color}
-                onChangeText={(val) => set_email(val)}/>
-            </View>
-
-            <View style={styles.login_field}>
-                <TextInput 
                 style={styles.input_text} 
                 placeholder='Username' 
                 placeholderTextColor={global_styles.background_color}
@@ -56,33 +48,54 @@ export default function signup_screen({navigation}){
                 onChangeText={(val) => set_password(val)}/>
             </View>
 
+            <View style={styles.login_field}>
+                <TextInput 
+                style={styles.input_text}
+                placeholder='Master Credential' 
+                placeholderTextColor={global_styles.background_color}
+                onChangeText={(val) => set_master_cred(val)}/>
+            </View>
+
             <TouchableOpacity style={styles.login_btn}
                 onPress={() => {
-                    {/*register_user(first_name, last_name, email, username, password)*/}
-                    navigation.navigate('home')
+                    register_user(first_name, last_name, master_cred, username, password, navigation)
                 }}>
                 <Text style={styles.login_text}>Sign Up</Text>
             </TouchableOpacity>
         </View>
     );
 };
-
 //Send POST request to create new user
-function register_user(first_name, last_name, email, username, password){
-    fetch('http://localhost:8000/user/new', {
+function register_user(first_name, last_name, master_cred, username, password, navigation){
+    var request_params = {
         method: 'POST',
+        headers: {
+            'Accept': "application/json, text/plain, */*",
+            'Content-Type': "application/json;charset=utf-8"
+        },
         body: JSON.stringify({
             username: username,
             password: password,
-            firstName: first_name,
-            lastName: last_name,
-            email: email
+            FirstName: first_name,
+            LastName: last_name,
+            MasterCred: master_cred
+        }),
+        redirect: 'follow'
+    };
+
+    fetch('http://73.66.169.37:8080/user/new', request_params)
+        .then((response) =>{
+            console.log('Response: ' + response.status);
+            switch(response.status){
+                case 200: //user registration succeeded
+                    //TODO: log user in before navigating to home page
+                    navigation.navigate('home');
+                    break;
+                case 400: //username already taken
+                    break;
+            }
         })
-    })
-    .then((response) =>{
-        console.log('response', response);
-    })
-    .catch( error =>{
-        console.error(error);
-    });
+        .catch( error =>{
+            console.error(error);
+        });
 }
