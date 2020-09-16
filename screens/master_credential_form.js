ff940d057ac5c4bbff8733cd754d64d34007a554
Roy Dey <roydey@ucdavis.edu>
2020-09-15 20:13:09 -0700

    get jwt trusted token after entering master credential

--- screens/master_credential_form.js
+++ screens/master_credential_form.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, Modal } from 'reac
 import  global_styles  from '../styles/global_styles'
 import { Formik } from 'formik'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import AsyncStorage from '@react-native-community/async-storage';
 
 const styles = global_styles.css_styles;
 export default function CredentialForm(){
@@ -11,7 +12,7 @@ export default function CredentialForm(){
             <Formik
                 initialValues={{master_credential:null}}
                 onSubmit={(values) => {
-                    console.log(values);
+                    get_jwt_trusted_token(values.master_credential)
                 }}
             >
                 {(props) => (
@@ -38,4 +39,57 @@ export default function CredentialForm(){
             </Formik>
         </View>
     )
+}
+
+const get_value = async(key) =>{
+    try {
+        return await AsyncStorage.getItem(key);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const store_value = async(key, value) =>{
+    try {
+        return value = await AsyncStorage.setItem(key, value)
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function get_jwt_trusted_token(master_credential){
+    //get refresh token
+    get_value('refresh_token').then( token => {
+        var refresh_token = token
+        //initialize request params
+        var request_params = {
+            method: 'POST',
+            headers: {
+                'Accept': "application/json, text/plain, */*",
+                'Content-Type': "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                refreshToken: refresh_token,
+                masterCred: master_credential
+            }),
+            redirect: 'follow'
+        };
+        console.log('refresh_token',refresh_token);
+        //get jwtTrusted token
+        fetch("http://73.66.169.37:8080/auth/refresh", request_params)
+            .then(response =>{
+                if(response.ok){
+                    return response.json();
+                }else{
+                    throw 'master credential incorrect';
+                }
+            })
+            .then(response =>{
+                console.log(response);
+                store_value('jwt_trusted', response.jwt);
+                store_value('jwt_trusted_timestamp', Date.now().toString());
+            })
+            .catch(error => console.log('Error:', error));
+
+    });
 }
