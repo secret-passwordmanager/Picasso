77d366f179da55a4b5f931eacb88ba8b33252af9
Martin <mlpetrov@ucdavis.edu>
2020-10-04 21:27:05 -0700

    started working on adding socket support

--- screens/requests.js
+++ screens/requests.js
@@ -1,15 +1,24 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStacknavigator} from '@react-navigation/stack'
-import global_styles from '../styles/global_styles'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStacknavigator} from '@react-navigation/stack';
+import global_styles from '../styles/global_styles';
+import Socket from '../utils/socket_io';
 
 const styles = global_styles.css_styles;
 
 export default function requests_screen({navigation}){
     return(
         <View style={styles.container}>
-            <Text style={styles.login_text}>Home Screen</Text>
+            <TouchableOpacity
+                onPress={
+                    () => {
+                        new Socket().connect();
+                    }
+                }
+            >
+        <Text>Connect</Text>
+            </TouchableOpacity>
         </View>
     );
 };
---
+++ utils/subscriber.js
@@ -0,0 +1,6 @@
+export default class Subscriber {
+
+    update(event, opts) {
+       return new Error('Derived class has not overridden update member function');
+    }
+ }
